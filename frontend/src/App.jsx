03ec66148087db8cc21a13414b8cee60de28b7ed
Douglas <capugougmor@gmail.com>
2021-05-04 22:18:43 -0300

    static charts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,33 @@
 import NavBar from "./componentes/BavBar/Index";
+import BarChart from "./componentes/Barchart/Index";
 import Footer from "./componentes/Footer/Index";
 import DataTable from "./componentes/Table/Index";
+import DonutChart from "./componentes/DonutChart/Index";
 
 function App() {
   return (
     <>
       <NavBar />
-      <h1 className="container">
-        <DataTable />
-      </h1>
+      <div className="container">
+        <h2 className="text-primary py-3">Dashboard de Vendas</h2>
+
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Taxa de sucesso</h5>
+            <BarChart />
+          </div>
+
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Todas as vendas</h5>
+            <DonutChart />
+          </div>
+        </div>
+
+        <div className="py-3">
+          <h2 className="text-primary">Todas as vendas</h2>
+        </div>
+
+      </div>
       <Footer />
     </>
   );
